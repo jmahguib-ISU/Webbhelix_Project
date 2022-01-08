509cac2d66a12fbf044fcd6a5747064d40765cf9
jmahguib <jmahguib@iastate.edu>
2022-01-07 23:16:55 -0800

    Uploaded files to be moved into in_n_out_files--Stacks_Pipeline directory

---
+++ Stacks_Populations_outputs_PART1/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080_output/Stacks_Populations_TrkNbr-1524n1282n1083_AllPops__p14_r080.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --ntasks=10
+#SBATCH --partition=speedy
+#SBATCH --nodes=1
+#SBATCH --mem=60G
+#SBATCH --time=30-00:00:00
+#SBATCH --error=/work/LAS/kjroe-lab/jmahguib/Stacks_Populations_outputs/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080_output/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080.%J.err
+#SBATCH --output=/work/LAS/kjroe-lab/jmahguib/Stacks_Populations_outputs/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080_output/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080.%J.out
+
+cd
+module load gcc/7.3.0-xegsmw4
+module load stacks/1.46-uyma7zn
+populations -P /work/LAS/kjroe-lab/jmahguib/UCSStacks_outputs/TrkNbr-1524n1282n1083_AllPops_UCSStacks_outputs -O /work/LAS/kjroe-lab/jmahguib/Stacks_Populations_outputs/TrkNbr-1524n1282n1083_AllPops_Stacks_Populations_p14_r080_output -M /work/LAS/kjroe-lab/jmahguib/PopMaps/Webbhelix/PopMap_TrkNbr-1524n1282n1083_AllPops.txt -b 22540000 -t 10 -k -p 14 -r 0.80 --write_single_snp --fstats --plink --structure --genepop --vcf --phylip_var
